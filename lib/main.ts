c64be4c1fa56c16857d97d607fb76decc25adc9e
WhyAsh5114 <whyash5114@gmail.com>
2024-10-21 18:41:49 +0530

    feat: add a dataType mapper

--- lib/main.ts
+++ lib/main.ts
@@ -1,3 +1,49 @@
-export function createTIDB(): string {
-  return "singleton";
+import prismaInternals from "@prisma/internals";
+
+type MappedDataModelEntry = {
+  modelName: string;
+  fieldName: string;
+  fieldType: string;
+  mappedFieldType: string;
+};
+
+const typeMapping = new Map([
+  ["String", "string"],
+  ["Int", "number"],
+  ["Float", "number"],
+  ["Boolean", "boolean"],
+  ["DateTime", "Date"],
+]);
+
+export async function createTIDB(
+  datamodelPath: string
+): Promise<MappedDataModelEntry[]> {
+  const dmmf = await prismaInternals.getDMMF({ datamodelPath });
+
+  const mappedDataModel: MappedDataModelEntry[] = dmmf.datamodel.models.flatMap(
+    (model) =>
+      model.fields.map((field) => {
+        const mappedDataModelEntry: MappedDataModelEntry = {
+          modelName: model.name,
+          fieldName: field.name,
+          fieldType: field.type,
+          mappedFieldType: "modelRelation",
+        };
+
+        if (field.kind === "object") {
+          return mappedDataModelEntry;
+        }
+
+        const mappedType = typeMapping.get(field.type);
+        if (!mappedType) {
+          throw new Error(`Prisma type ${field.type} is not yet supported`);
+        }
+
+        mappedDataModelEntry.mappedFieldType = mappedType;
+        return mappedDataModelEntry;
+      })
+  );
+  return mappedDataModel;
 }
+
+console.table(await createTIDB("./lib/prisma/schema.prisma"));
